3ef58abf7a9ddf32e70894bf324522af656378a6
Chitransh Saxena <chitransh08642@gmail.com>
2024-04-26 16:32:32 +0530

    Add existing file

--- program1.js
+++ program1.js
@@ -4,8 +4,26 @@
  */
 var isValid = function(s) {
     
+    const stack = [];
+    
+    const bracketMap = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    
+    for (let char of s) {
+        if (char in bracketMap) {
+            if (stack.length === 0 || stack.pop() !== bracketMap[char]) {
+                return false; 
+            }
+        } else {       
+            stack.push(char);
+        }
+    }    
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
 
